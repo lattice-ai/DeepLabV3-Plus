b0a9dc35e9474c0cacf8b1f3a751f0c509999e4b
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-18 23:25:59 +0530

    Added Middle Flow (Aligned Exception)

--- src/backbones/__init__.py
+++ src/backbones/__init__.py
@@ -1,14 +1,17 @@
 from .aligned_xception import (
     ConvolutionBlock,
     DepthWiseConvolutionBlock,
-    EntryFlowBlock
+    EntryFlowBlock,
+    MiddleFlowBlock
 )
 
 
 def AlignedException(input_tensor, block_prefix='Aligned_Exception'):
-    y = ConvolutionBlock(input_tensor, 32, block_prefix=block_prefix + 'Entry_Flow_Conv_1')
-    y = ConvolutionBlock(y, 64, stride=1, block_prefix=block_prefix + 'Entry_Flow_Conv_2')
-    y = EntryFlowBlock(y, 128, block_prefix=block_prefix + 'Entry_Flow_Block_1')
-    y = EntryFlowBlock(y, 256, block_prefix=block_prefix + 'Entry_Flow_Block_2')
-    y = EntryFlowBlock(y, 728, block_prefix=block_prefix + 'Entry_Flow_Block_3')
+    y = ConvolutionBlock(input_tensor, 32, block_prefix=block_prefix + '_Entry_Flow_Conv_1')
+    y = ConvolutionBlock(y, 64, stride=1, block_prefix=block_prefix + '_Entry_Flow_Conv_2')
+    y = EntryFlowBlock(y, 128, block_prefix=block_prefix + '_Entry_Flow_Block_1')
+    y = EntryFlowBlock(y, 256, block_prefix=block_prefix + '_Entry_Flow_Block_2')
+    y = EntryFlowBlock(y, 728, block_prefix=block_prefix + '_Entry_Flow_Block_3')
+    for i in range(1, 17):
+        y = MiddleFlowBlock(y, block_prefix=block_prefix + '_Middle_Flow_' + str(i))
     return y
