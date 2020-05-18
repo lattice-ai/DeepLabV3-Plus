19cbac099bb81557f282e63e74f6a7b24e844014
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-18 21:44:01 +0530

    Added DepthWise Convolution Block (Aligned Xception)

--- src/backbones/aligned_xception.py
+++ src/backbones/aligned_xception.py
@@ -30,3 +30,43 @@ def ConvolutionBlock(
         name=block_prefix + '_Activation'
     )(y) if use_activation else y
     return y
+
+
+def DepthWiseConvolutionBlock(
+        input_tensor, filters, kernel_size=3,
+        stride=1, rate=1, block_prefix=None):
+    padding = None
+    y = input_tensor
+    if stride == 1:
+        padding = 'same'
+    else:
+        kernel_size_effective = kernel_size + (kernel_size - 1) * (rate - 1)
+        pad_total = kernel_size_effective - 1
+        pad_beg = pad_total // 2
+        pad_end = pad_total - pad_beg
+        y = tf.keras.layers.ZeroPadding2D((pad_beg, pad_end))(y)
+        padding = 'valid'
+    y = tf.keras.layers.DepthwiseConv2D(
+        (kernel_size, kernel_size), strides=(stride, stride),
+        dilation_rate=(rate, rate), padding=padding,
+        use_bias=False, name=block_prefix + '_DepthwiseConv2D'
+    )(y)
+    y = tf.keras.layers.BatchNormalization(
+        name=block_prefix + '_BatchNormalization'
+    )(y)
+    y = tf.keras.layers.Activation(
+        tf.nn.relu,
+        name=block_prefix + '_Activation'
+    )(y)
+    y = tf.keras.layers.Conv2D(
+        filters, (1, 1), padding='same', use_bias=False,
+        name=block_prefix + '_Pointwise_Conv2D'
+    )(y)
+    y = tf.keras.layers.BatchNormalization(
+        name=block_prefix + '_BatchNormalization'
+    )(y)
+    y = tf.keras.layers.Activation(
+        tf.nn.relu,
+        name=block_prefix + '_Activation'
+    )(y)
+    return y
