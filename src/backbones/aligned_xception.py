db9f82e955e712bfea1a52c853aa446749bbeb03
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-18 19:59:34 +0530

    Added Convolution Block (Aligned Xception)

---
+++ src/backbones/aligned_xception.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+def ConvolutionBlock(
+        input_tensor, filters, kernel_size=(3, 3),
+        strides=(2, 2), use_bias=False, use_bn=True,
+        use_activation=True, block_prefix=None):
+    y = tf.keras.layers.Conv2D(
+        filters, kernel_size, strides,
+        use_bias=use_bias, name=block_prefix + '_Conv2D'
+    )(input_tensor)
+    y = tf.keras.layers.BatchNormalization(
+        name=block_prefix + '_BatchNormalization'
+    )(y) if use_bn else y
+    y = tf.keras.layers.Activation(
+        'relu', name=block_prefix + '_Activation'
+    )(y) if use_activation else y
+    return y
