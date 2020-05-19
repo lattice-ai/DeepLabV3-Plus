f7f5105a98ca23f0b10ca20c52b087f51cb24e67
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-19 11:16:23 +0530

    Added ASPP Block

--- src/blocks.py
+++ src/blocks.py
@@ -15,3 +15,56 @@ def ASPPConvBlock(
         tf.nn.relu, name=block_prefix + '_Activation'
     )(y)
     return y
+
+
+def AtrousSpatialPyramidPooling(input_tensor, block_prefix):
+    input_shape = tf.keras.backend.int_shape(input_tensor)
+    # 1x1 Conv
+    y_1x1 = ASPPConvBlock(
+        input_tensor, 256, kernel_size=1,
+        block_prefix=block_prefix + '_1x1_Conv_Block'
+    )
+    # 3x3 Conv, rate 6
+    y_3x3_r6 = ASPPConvBlock(
+        input_tensor, 256, kernel_size=3, rate=6,
+        block_prefix=block_prefix + '_3x3_Conv_Block_Rate_6'
+    )
+    # 3x3 Conv, rate 12
+    y_3x3_r12 = ASPPConvBlock(
+        input_tensor, 256, kernel_size=3, rate=12,
+        block_prefix=block_prefix + '_3x3_Conv_Block_Rate_12'
+    )
+    # 3x3 Conv, rate 18
+    y_3x3_r18 = ASPPConvBlock(
+        input_tensor, 256, kernel_size=3, rate=18,
+        block_prefix=block_prefix + '_3x3_Conv_Block_Rate_18'
+    )
+    # Image Pooling
+    y_image_pooling = tf.keras.layers.AveragePooling2D(
+        pool_size=(
+            input_shape[1],
+            input_shape[2]
+        ),
+        name=block_prefix + '_Average_Pooling'
+    )(input_tensor)
+    y_image_pooling = ASPPConvBlock(
+        y_image_pooling, 256, kernel_size=1,
+        name=block_prefix + '_Image_Pooling_Conv_Block'
+    )
+    y_image_pooling = tf.keras.layers.UpSampling2D(
+        y_image_pooling,
+        size=[input_shape[1], input_shape[2]],
+        interpolation='bilinear'
+    )(y_image_pooling)
+    # Concatenation
+    y = tf.keras.layers.Concatenate(
+        name=block_prefix + '_Concatenate'
+    )([
+        y_1x1, y_3x3_r6, y_3x3_r12,
+        y_3x3_r18, y_image_pooling
+    ])
+    y = ASPPConvBlock(
+        y, 256, kernel_size=1, rate=1,
+        block_prefix=block_prefix + '_Final_ASPP_Conv_Block'
+    )
+    return y
