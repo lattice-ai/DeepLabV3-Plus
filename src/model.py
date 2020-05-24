4acd092aae37087a4b275db64683375401d5fdd8
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-24 19:10:26 +0530

    Added Decoder

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ from .backbones import AlignedException, get_resnet_101
 from .blocks import AtrousSpatialPyramidPooling, BilinearUpsample
 
 
-def DeepLabV3Plus(input_shape=(512, 512, 3), backbone='resnet101', model_name='DeepLabV3Plus'):
+def DeepLabV3Plus(input_shape=(512, 512, 3), backbone='resnet101', n_classes=66, model_name='DeepLabV3Plus'):
     # Encoder
     if backbone == 'resnet101':
         backbone = get_resnet_101(input_shape=input_shape, weights=None)
@@ -34,9 +34,38 @@ def DeepLabV3Plus(input_shape=(512, 512, 3), backbone='resnet101', model_name='D
     decoder_y = tf.keras.layers.Activation(
         tf.nn.relu, name='Decoder_Activation_1'
     )(decoder_y)
-    print(tf.keras.backend.int_shape(encoder_y), tf.keras.backend.int_shape(decoder_y))
     y = tf.keras.layers.Concatenate(
         name='Encoder_Decoder_Concatenation'
     )([encoder_y, decoder_y])
+    y = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=3, padding='same',
+        activation='relu', name='Decoder_Conv_3x3_2', use_bias=False
+    )(y)
+    y = tf.keras.layers.BatchNormalization(
+        name='Decoder_BacthNorm_2'
+    )(y)
+    y = tf.keras.layers.Activation(
+        tf.nn.relu, name='Decoder_Activation_2'
+    )(y)
+    y = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=3, padding='same',
+        activation='relu', name='Decoder_Conv_3x3_3', use_bias=False
+    )(y)
+    y = tf.keras.layers.BatchNormalization(
+        name='Decoder_BacthNorm_3'
+    )(y)
+    y = tf.keras.layers.Activation(
+        tf.nn.relu, name='Decoder_Activation_3'
+    )(y)
+    y = BilinearUpsample(
+        y, [input_shape[0], input_shape[1]],
+        name='Decoder_Final_Upsample'
+    )
+    y = tf.keras.layers.Conv2D(
+        n_classes, (1, 1), name='Output_Conv2D'
+    )(y)
+    y = tf.keras.layers.Activation(
+        tf.nn.softmax, name='Output_Activation'
+    )(y)
     model = tf.keras.Model(input_tensor, y, name=model_name)
     return model
