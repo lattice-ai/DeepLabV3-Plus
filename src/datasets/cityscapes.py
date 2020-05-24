333e462a7c087ab1880e770c1e51753e0484c8e1
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-24 20:28:01 +0530

    Added Image reading function

--- src/datasets/cityscapes.py
+++ src/datasets/cityscapes.py
@@ -21,3 +21,27 @@ class CityscapesDataet:
             assert self.val_image_list[i].split('/')[-1].split('_leftImg8bit')[0] == \
                    self.val_mask_list[i].split('/')[-1].split('_gtFine_labelIds')[0]
         print('Validation Directories Good to go')
+
+    @staticmethod
+    def read_image(image_path, img_height=800, img_width=1600, mask=False, flip=0):
+        image = tf.io.read_file(image_path)
+        if not mask:
+            image = tf.cast(tf.image.decode_png(image, channels=3), dtype=tf.float32)
+            image = tf.image.resize(images=image, size=[img_height, img_width])
+            image = tf.image.random_brightness(image, max_delta=50.)
+            image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+            image = tf.image.random_hue(image, max_delta=0.2)
+            image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+            image = tf.clip_by_value(image, 0, 255)
+            image = tf.case([
+                (tf.greater(flip, 0), lambda: tf.image.flip_left_right(image))
+            ], default=lambda: image)
+            image = image[:, :, ::-1] - tf.constant([103.939, 116.779, 123.68])
+        else:
+            image = tf.image.decode_png(image, channels=1)
+            image = tf.cast(tf.image.resize(images=image, size=[
+                img_height, img_width]), dtype=tf.uint8)
+            image = tf.case([
+                (tf.greater(flip, 0), lambda: tf.image.flip_left_right(image))
+            ], default=lambda: image)
+        return image
