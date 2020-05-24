a04db78cbbcf2b19ec577f1e6b700dd92fc1b182
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-24 23:10:30 +0530

    Added Trainer

--- train.py
+++ train.py
@@ -7,9 +7,10 @@ class Trainer:
 
     def __init__(self, config):
         self.config = config
-        cityscapes_dataset = CityscapesDataet(self.config['dataset_config'])
-        self.train_dataset, self.val_dataset = cityscapes_dataset.get_datasets()
         self.strategy = self.config['strategy']
+        with self.strategy.scope():
+            self.cityscapes_dataset = CityscapesDataet(self.config['dataset_config'])
+            self.train_dataset, self.val_dataset = self.cityscapes_dataset.get_datasets()
         self.model = self.define_model()
 
     def define_model(self):
@@ -21,8 +22,8 @@ class Trainer:
             )
             for layer in model.layers:
                 if isinstance(layer, tf.keras.layers.BatchNormalization):
-                    layer.momentum = 0.9997
-                    layer.epsilon = 1e-5
+                    layer.momentum = self.config['bn_momentum'],
+                    layer.epsilon = self.config['bn_epsilon']
                 elif isinstance(layer, tf.keras.layers.Conv2D):
                     layer.kernel_regularizer = tf.keras.regularizers.l2(1e-4)
             model.compile(
@@ -30,3 +31,29 @@ class Trainer:
                 optimizer=tf.optimizers.Adam(learning_rate=1e-4), metrics=['accuracy']
             )
         return model
+
+    def train(self):
+        train_steps_per_epoch = len(
+            self.cityscapes_dataset.train_image_list
+        ) // self.config['batch_size']
+        val_steps_per_epoch = len(
+            self.cityscapes_dataset.val_image_list
+        ) // self.config['batch_size']
+        history = self.model.fit(
+            self.train_dataset,
+            steps_per_epoch=train_steps_per_epoch,
+            epochs=self.config['epochs'],
+            validation_data=self.val_dataset,
+            validation_steps=val_steps_per_epoch,
+            callbacks=[
+                tf.keras.callbacks.TensorBoard(
+                    log_dir='logs', write_graph=True, update_freq='batch'
+                ),
+                tf.keras.callbacks.ModelCheckpoint(
+                    mode='min', filepath=self.config['weight_file'],
+                    monitor='val_loss', save_best_only='True',
+                    save_weights_only='True', verbose=1
+                )
+            ]
+        )
+        return history
