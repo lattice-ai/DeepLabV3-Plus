02896695d4159a24929df70351f8d1de6f5ca3cb
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-24 18:46:32 +0530

    Updated Encoder Implementation

--- src/model.py
+++ src/model.py
@@ -1,25 +1,26 @@
 import tensorflow as tf
-from .backbones import AlignedException
-from .blocks import AtrousSpatialPyramidPooling
+from .backbones import AlignedException, get_resnet_101
+from .blocks import AtrousSpatialPyramidPooling, BilinearUpsample
 
 
-def DeepLabV3Plus(input_shape=(512, 512, 3), model_name='DeepLabV3Plus'):
+def DeepLabV3Plus(input_shape=(512, 512, 3), backbone='resnet101', model_name='DeepLabV3Plus'):
     # Encoder
-    input_tensor = tf.keras.Input(
-        shape=input_shape,
-        name=model_name + '_Input'
+    if backbone == 'resnet101':
+        backbone = get_resnet_101(input_shape=input_shape, weights=None)
+        input_tensor = backbone.input
+        y = AtrousSpatialPyramidPooling(backbone.get_layer('conv5_block3_out').output)
+    elif backbone == 'alignedexception' or backbone == 'exception':
+        input_tensor = tf.keras.Input(
+            shape=input_shape,
+            name=model_name + '_Input'
+        )
+        backbone = AlignedException(input_tensor)
+        y = AtrousSpatialPyramidPooling(backbone)
+    y = BilinearUpsample(
+        tensor=y, size=[
+            input_shape[0] // 4,
+            input_shape[1] // 4
+        ], name='Encoder_Output_Upsample'
     )
-    backbone = AlignedException(input_tensor)
-    y = AtrousSpatialPyramidPooling(backbone)
-    # Decoder
-    encoder_output_shape = tf.keras.backend.int_shape(y)
-    y = tf.keras.layers.UpSampling2D(
-        [
-            encoder_output_shape[1] * 4,
-            encoder_output_shape[2] * 4
-        ],
-        interpolation='bilinear',
-        name=model_name + '_Encoder_Output_UpSampling'
-    )(y)
     model = tf.keras.Model(input_tensor, y, name=model_name)
     return model
