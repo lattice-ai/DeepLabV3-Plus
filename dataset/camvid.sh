190ace689a74cb7fd727fd9e3867ef30ce4ad82e
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-25 23:23:48 +0530

    Updated Camvid setup script

--- dataset/camvid.sh
+++ dataset/camvid.sh
@@ -1,5 +1,6 @@
 mkdir /root/.kaggle
 touch /root/.kaggle/kaggle.json
+chmod 600 /root/.kaggle/kaggle.json
 echo "{\"username\":\"<YOUR-USERNAME>\",\"key\":\"<YOUR-KAGGLE-KEY>\"}" > /root/.kaggle/kaggle.json
 kaggle datasets download -d jcoral02/camvid
 unzip -q ./camvid.zip
