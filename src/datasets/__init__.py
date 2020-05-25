e40719177f401100c8c186a425dc9e583a0394ed
soumik12345 <19soumik.rakshit96@gmail.com>
2020-05-25 22:45:33 +0530

    Updated Base Dataset Class

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -20,12 +20,12 @@ class Dataset:
     def assert_dataset(self):
         pass
 
-    @staticmethod
-    def read_image(image_path, img_height=800, img_width=1600, mask=False, flip=0):
+    def read_image(self, image_path, img_height=800, img_width=1600, mask=False, flip=0):
         image = tf.io.read_file(image_path)
         if not mask:
             image = tf.cast(tf.image.decode_png(image, channels=3), dtype=tf.float32)
-            image = tf.image.resize(images=image, size=[img_height, img_width])
+            if img_height is None and img_width is None:
+                image = tf.image.resize(images=image, size=[img_height, img_width])
             image = tf.image.random_brightness(image, max_delta=50.)
             image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
             image = tf.image.random_hue(image, max_delta=0.2)
@@ -37,8 +37,9 @@ class Dataset:
             image = image[:, :, ::-1] - tf.constant([103.939, 116.779, 123.68])
         else:
             image = tf.image.decode_png(image, channels=1)
-            image = tf.cast(tf.image.resize(images=image, size=[
-                img_height, img_width]), dtype=tf.uint8)
+            if img_height is None and img_width is None:
+                image = tf.image.resize(images=image, size=[img_height, img_width])
+            image = tf.cast(image, dtype=tf.uint8)
             image = tf.case([
                 (tf.greater(flip, 0), lambda: tf.image.flip_left_right(image))
             ], default=lambda: image)
