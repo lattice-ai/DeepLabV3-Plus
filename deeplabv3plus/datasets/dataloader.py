7fdab7efba72b4659d8926cca2b0f2232b190b88
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-16 19:22:52 +0530

    Made label resizing method as neares

--- deeplabv3plus/datasets/dataloader.py
+++ deeplabv3plus/datasets/dataloader.py
@@ -24,7 +24,7 @@ class GenericDataLoader:
                 images=image, size=[
                     self.configs['height'],
                     self.configs['width']
-                ]
+                ], method="nearest"
             ))
             image = tf.cast(image, tf.float32)
         else:
