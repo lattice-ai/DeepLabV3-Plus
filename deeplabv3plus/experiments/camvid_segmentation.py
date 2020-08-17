a83c106d64610dc1534dc7ce4f76d3eae1448c82
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-17 20:59:51 +0530

    Updated camvid segmentation configs

--- deeplabv3plus/experiments/camvid_segmentation.py
+++ deeplabv3plus/experiments/camvid_segmentation.py
@@ -6,7 +6,7 @@ from ..train import Trainer, tf
 
 def run_training():
     config = {
-        'wandb_api_key': 'kjbckajsbdksjbdkajsbkdasbkdj',
+        'wandb_api_key': '60cfb770bc5de44f181c8cb5e270eb266bc918c6',
         'project_name': 'deeplabv3-plus',
         'experiment_name': 'camvid-segmentation-resnet-50-backbone',
         'train_dataset_configs': {
@@ -20,10 +20,10 @@ def run_training():
             'height': 512, 'width': 512, 'batch_size': 8
         },
         'strategy': tf.distribute.OneDeviceStrategy(device="/gpu:0"),
-        'num_classes': 20, 'height': 360, 'width': 480,
+        'num_classes': 12, 'height': 512, 'width': 512,
         'backbone': 'resnet50', 'learning_rate': 0.0001,
         'checkpoint_path': os.path.join(
-            wandb.run.dir,
+            './checkpoints',
             'deeplabv3-plus-camvid-segmentation-resnet-50-backbone.h5'
         ),
         'epochs': 100
