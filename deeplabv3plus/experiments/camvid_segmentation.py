04a6c56ebbe2c90ca2a27d61a8b38aa45edbace6
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-17 17:47:36 +0530

    Updated Camvid Segmentation Experiment

--- deeplabv3plus/experiments/camvid_segmentation.py
+++ deeplabv3plus/experiments/camvid_segmentation.py
@@ -3,19 +3,20 @@ import wandb
 from glob import glob
 from ..train import Trainer, tf
 
+
 def run_training():
     config = {
         'wandb_api_key': 'kjbckajsbdksjbdkajsbkdasbkdj',
         'project_name': 'deeplabv3-plus',
         'experiment_name': 'camvid-segmentation-resnet-50-backbone',
         'train_dataset_configs': {
-            'images': sorted(glob('./camvid/train/*')),
-            'labels': sorted(glob('./camvid/trainannot/*')),
+            'images': sorted(glob('./dataset/camvid/train/*')),
+            'labels': sorted(glob('./dataset/camvid/trainannot/*')),
             'height': 360, 'width': 480, 'batch_size': 8
         },
         'val_dataset_configs': {
-            'images': sorted(glob('./camvid/val/*')),
-            'labels': sorted(glob('./camvid/valannot/*')),
+            'images': sorted(glob('./dataset/camvid/val/*')),
+            'labels': sorted(glob('./dataset/camvid/valannot/*')),
             'height': 512, 'width': 512, 'batch_size': 8
         },
         'strategy': tf.distribute.OneDeviceStrategy(device="/gpu:0"),
