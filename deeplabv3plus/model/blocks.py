2b5fa145e01198717e091411c6afd1ee242dd5f5
Arindam Das <dasarindam.mails@gmail.com>
2020-08-18 20:30:06 +0530

    Refactored function to tf.keras.Model hierarchy

--- deeplabv3plus/model/blocks.py
+++ deeplabv3plus/model/blocks.py
@@ -1,72 +1,113 @@
+"""Module providing building blocks for the DeepLabV3+ netowork architecture.
+"""
+
 import tensorflow as tf
-from tensorflow.keras import backend as K
 
 
-def ConvBlock(
-    x, n_filters, kernel_size, padding,
-    dilation_rate, kernel_initializer,
-    use_bias=True, conv_activation=None):
-    x = tf.keras.layers.Conv2D(
-        n_filters, kernel_size=kernel_size, padding=padding,
-        kernel_initializer=kernel_initializer, use_bias=use_bias,
-        dilation_rate=dilation_rate, activation=conv_activation
-    )(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.ReLU()(x)
-    return x
+class ConvBlock(tf.keras.layers.Layer):
+    """Convolutional Block for DeepLabV3+
+
+    Convolutional block consisting of Conv2D -> BatchNorm -> ReLU
+
+    Args:
+        n_filters:
+            number of output filters
+        kernel_size:
+            kernel_size for convolution
+        padding:
+            padding for convolution
+        kernel_initializer:
+            kernel initializer for convolution
+        use_bias:
+            boolean, whether of not to use bias in convolution
+        dilation_rate:
+            dilation rate for convolution
+        activation:
+            activation to be used for convolution
+    """
+    # !pylint:disable=too-many-arguments
+    def __init__(self, n_filters, kernel_size, padding, dilation_rate,
+                 kernel_initializer, use_bias, conv_activation=None):
+        super(ConvBlock, self).__init__()
+
+        self.conv = tf.keras.layers.Conv2D(
+            n_filters, kernel_size=kernel_size, padding=padding,
+            kernel_initializer=kernel_initializer,
+            use_bias=use_bias, dilation_rate=dilation_rate,
+            activation=conv_activation)
+
+        self.batch_norm = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.ReLU()
+
+    def call(self, inputs, **kwargs):
+        tensor = self.conv(inputs)
+        tensor = self.batch_norm(tensor)
+        tensor = self.relu(tensor)
+        return tensor
 
 
-def AtrousSpatialPyramidPooling(input_tensor):
-    dims = K.int_shape(input_tensor)
+class AtrousSpatialPyramidPooling(tf.keras.layers.Layer):
+    """Atrous Spatial Pyramid Pooling layer for DeepLabV3+ architecture."""
+    # !pylint:disable=too-many-instance-attributes
+    def __init__(self):
+        super(AtrousSpatialPyramidPooling, self).__init__()
 
-    x = tf.keras.layers.AveragePooling2D(
-        pool_size=(dims[-3], dims[-2])
-    )(input_tensor)
+        # layer architecture components
+        self.avg_pool = None
+        self.conv1, self.conv2 = None, None
+        self.pool = None
+        self.out1, self.out6, self.out12, self.out18 = None, None, None, None
 
-    x = ConvBlock(
-        x, 256, kernel_size=1, padding='same',dilation_rate=1,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
+    @staticmethod
+    def _get_conv_block(kernel_size, dilation_rate, use_bias=False):
+        return ConvBlock(256,
+                         kernel_size=kernel_size,
+                         dilation_rate=dilation_rate,
+                         padding='same',
+                         use_bias=use_bias,
+                         kernel_initializer=tf.keras.initializers.he_normal())
 
-    pool = tf.keras.layers.UpSampling2D(
-        size=(
-            dims[-3] // x.shape[1],
-            dims[-2] // x.shape[2]
-        ),
-        interpolation='bilinear'
-    )(x)
+    def build(self, input_shape):
+        dummy_tensor = tf.random.normal(input_shape)  # used for calculating
+        # output shape of convolutional layers
 
-    out_1 = ConvBlock(
-        input_tensor, 256, kernel_size=1,
-        dilation_rate=1, padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
+        self.avg_pool = tf.keras.layers.AveragePooling2D(
+            pool_size=(input_shape[-3], input_shape[-2]))
 
-    out_6 = ConvBlock(
-        input_tensor, 256, kernel_size=3,
-        dilation_rate=6, padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
+        self.conv1 = AtrousSpatialPyramidPooling._get_conv_block(
+            kernel_size=1, dilation_rate=1, use_bias=True)
 
-    out_12 = ConvBlock(
-        input_tensor, 256, kernel_size=3,
-        dilation_rate=12, padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
+        self.conv2 = AtrousSpatialPyramidPooling._get_conv_block(
+            kernel_size=1, dilation_rate=1)
 
-    out_18 = ConvBlock(
-        input_tensor, 256, kernel_size=3,
-        dilation_rate=18, padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
+        dummy_tensor = self.conv1(self.avg_pool(dummy_tensor))
 
-    x = tf.keras.layers.Concatenate(axis=-1)([
-        pool, out_1, out_6, out_12, out_18
-    ])
+        self.pool = tf.keras.layers.UpSampling2D(
+            size=(
+                input_shape[-3] // dummy_tensor.shape[1],
+                input_shape[-2] // dummy_tensor.shape[2]
+            ),
+            interpolation='bilinear'
+        )
 
-    x = ConvBlock(
-        x, 256, kernel_size=1,
-        dilation_rate=1, padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal()
-    )
-    return x
+        self.out1, self.out6, self.out12, self.out18 = map(
+            AtrousSpatialPyramidPooling._get_conv_block,
+            [(1, 1), (3, 6), (3, 12), (3, 18)]
+        )
+
+    def call(self, inputs, **kwargs):
+        tensor = self.avg_pool(inputs)
+        tensor = self.conv1(tensor)
+        tensor = tf.keras.layers.Concatenate(axis=-1)([
+            self.pool(tensor),
+            self.out1(tensor),
+            self.out6(tensor),
+            self.out12(
+                tensor
+            ),
+            self.out18(
+                tensor
+            )
+        ])
+        tensor = self.conv2(tensor)
+        return tensor
--- deeplabv3plus/model/deeplabv3_plus.py
+++ deeplabv3plus/model/deeplabv3_plus.py
@@ -1,54 +1,102 @@
+"""Module providing the DeeplabV3+ network architecture as a tf.keras.Model.
+"""
+
 import tensorflow as tf
+
 from .backbones import BACKBONES
-from .blocks import AtrousSpatialPyramidPooling, ConvBlock
+from .blocks import (AtrousSpatialPyramidPooling,
+                     ConvBlock)
 
 
-def DeeplabV3Plus(num_classes, height, width, backbone='resnet50'):
-    model_input = tf.keras.Input(shape=(height, width, 3))
-    backbone_model = BACKBONES[backbone]['model'](
-        weights='imagenet',
-        include_top=False,
-        input_tensor=model_input
-    )
-    layer = backbone_model.get_layer(BACKBONES[backbone]['feature_1']).output
-    layer = AtrousSpatialPyramidPooling(layer)
-    input_a = tf.keras.layers.UpSampling2D(
-        size=(
-            height // 4 // layer.shape[1],
-            width // 4 // layer.shape[2]
-        ),
-        interpolation='bilinear'
-    )(layer)
+# !pylint:disable=too-many-ancestors, too-many-instance-attributes
+class DeeplabV3Plus(tf.keras.Model):
+    """DeeplabV3+ network architecture provider tf.keras.Model implementation.
 
-    input_b = backbone_model.get_layer(BACKBONES[backbone]['feature_2']).output
-    input_b = ConvBlock(
-        input_b, 48, kernel_size=(1, 1), padding='same', use_bias=False,
-        kernel_initializer=tf.keras.initializers.he_normal(), dilation_rate=1
-    )
+    Args:
+        num_classes:
+            number of segmentation classes, effectively - number of output
+            filters
+        height, width:
+            expected height, width of image
+        backbone:
+            backbone to be used
+    """
+    def __init__(self, num_classes, height, width, backbone='resnet50'):
+        super(DeeplabV3Plus, self).__init__()
 
-    layer = tf.keras.layers.Concatenate(axis=-1)([input_a, input_b])
+        self.num_classes = num_classes
+        self.height, self.width = height, width
+        self.backbone = backbone
+        self.aspp = None
+        self.input_b_conv, self.conv1, self.conv2 = None, None, None
+        self.out_conv = None
 
-    layer = ConvBlock(
-        layer, 256, kernel_size=3, padding='same', conv_activation='relu',
-        kernel_initializer=tf.keras.initializers.he_normal(), use_bias=False, dilation_rate=1
-    )
+        self._built = False
 
-    layer = ConvBlock(
-        layer, 256, kernel_size=3, padding='same', conv_activation='relu',
-        kernel_initializer=tf.keras.initializers.he_normal(), use_bias=False, dilation_rate=1
-    )
+    def _build(self):
+        if self._built:
+            return
 
-    layer = tf.keras.layers.UpSampling2D(
-        size=(
-            height // layer.shape[1],
-            width // layer.shape[2]
-        ),
-        interpolation='bilinear'
-    )(layer)
-    model_output = tf.keras.layers.Conv2D(
-        num_classes, kernel_size=(1, 1), padding='same'
-    )(layer)
+        self._built = True
 
-    model = tf.keras.Model(inputs=model_input, outputs=model_output)
+        self.aspp = AtrousSpatialPyramidPooling()
 
-    return model
+        self.input_b_conv = ConvBlock(
+            48, kernel_size=(1, 1), padding='same',
+            kernel_initializer=tf.keras.initializers.he_normal(),
+            use_bias=False, dilation_rate=1)
+
+        self.conv1 = ConvBlock(
+            256, kernel_size=3, padding='same', conv_activation='relu',
+            kernel_initializer=tf.keras.initializers.he_normal(),
+            use_bias=False, dilation_rate=1)
+
+        self.conv2 = ConvBlock(
+            256, kernel_size=3, padding='same', conv_activation='relu',
+            kernel_initializer=tf.keras.initializers.he_normal(),
+            use_bias=False, dilation_rate=1)
+
+        self.out_conv = tf.keras.layers.Conv2D(self.num_classes,
+                                               kernel_size=(1, 1),
+                                               padding='same')
+
+    def call(self, inputs, training=None, mask=None):
+        self._build()
+
+        inputs = tf.keras.Input(shape=(self.height, self.width, 3),
+                                tensor=inputs)
+
+        backbone_model = BACKBONES[self.backbone]['model'](
+            weights='imagenet', input_tensor=inputs)
+
+        tensor = backbone_model.get_layer(
+            BACKBONES[self.backbone]['feature_1']).output
+        tensor = self.aspp(tensor)
+
+        input_a = tf.keras.layers.UpSampling2D(
+            size=(
+                self.height
+                // 4 // tensor.shape[1],
+                self.width
+                // 4 // tensor.shape[2]
+            ),
+            interpolation='bilinear'
+        )(tensor)
+
+        input_b = backbone_model.get_layer(
+            BACKBONES[self.backbone]['feature_2']).output
+        input_b = self.input_b_conv(input_b)
+
+        tensor = tf.keras.layers.Concatenate(axis=-1)([input_a, input_b])
+        tensor = self.conv2(self.conv1(tensor))
+
+        tensor = tf.keras.layers.UpSampling2D(
+            size=(
+                self.height
+                // tensor.shape[1],
+                self.width
+                // tensor.shape[2]
+            ),
+            interpolation='bilinear'
+        )
+        return self.out_conv(tensor)
