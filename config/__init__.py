8276de3d59f0b1b3214ac7d4d02c6f5e63ad4a08
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-21 20:22:29 +0530

    Updated CONFIG_MAP

--- config/__init__.py
+++ config/__init__.py
@@ -6,10 +6,18 @@ entry in the CONFIG_MAP as shown.
 """
 
 import config.camvid_resnet50
+import config.camvid_resnet101
+import config.camvid_mobilenetv2
 import config.human_parsing_resnet50
+import config.human_parsing_resnet101
+import config.human_parsing_mobilenetv2
 
 
 CONFIG_MAP = {
     'camvid_resnet50': config.camvid_resnet50.CONFIG,
-    'human_parsing_resnet50': config.human_parsing_resnet50
+    'camvid_resnet101': config.camvid_resnet101.CONFIG,
+    'camvid_mobilenetv2': config.camvid_mobilenetv2.CONFIG,
+    'human_parsing_resnet50': config.human_parsing_resnet50.CONFIG,
+    'human_parsing_resnet101': config.human_parsing_resnet101.CONFIG,
+    'human_parsing_mobilenetv2': config.human_parsing_mobilenetv2.CONFIG,
 }
