2590df592beabbfa84d330927e7a15410ae6dea0
soumik12345 <19soumik.rakshit96@gmail.com>
2021-03-18 21:04:22 +0530

    Removed seed from sugmentations

--- deeplabv3plus/datasets/tfrecords/augmentations.py
+++ deeplabv3plus/datasets/tfrecords/augmentations.py
@@ -8,23 +8,22 @@ class AugmentationFactory:
         self.apply_jitter = apply_jitter
         self.image_channels, self.label_channels = 3, 1
 
-    def _flip_horizontal(self, image, label, seed):
+    def _flip_horizontal(self, image, label):
         comb_tensor = tf.concat([image, label], axis=2)
-        comb_tensor = tf.image.random_flip_left_right(comb_tensor, seed=seed)
+        comb_tensor = tf.image.random_flip_left_right(comb_tensor)
         image, label = tf.split(comb_tensor, [self.image_channels, self.label_channels], axis=-1)
         return image, label
 
     @staticmethod
-    def _random_jitter(image, seed):
-        image = tf.image.stateless_random_saturation(image, 0.9, 1.1, seed)
-        image = tf.image.stateless_random_brightness(image, 0.075, seed)
-        image = tf.image.stateless_random_contrast(image, 0.9, 1.1, seed)
+    def _random_jitter(image):
+        image = tf.image.stateless_random_saturation(image, 0.9, 1.1)
+        image = tf.image.stateless_random_brightness(image, 0.075)
+        image = tf.image.stateless_random_contrast(image, 0.9, 1.1)
         return image
 
     def _map_augmentations(self, image, label):
-        seed = tf.random.uniform((2,), minval=0, maxval=100, dtype=tf.int32)
         if self.apply_horizontal_flip:
-            image = self._flip_horizontal(image=image, label=label, seed=seed)
+            image = self._flip_horizontal(image=image, label=label)
         image = self._random_jitter(image=image, seed=seed) if self.apply_jitter else image
         return image, label
 
