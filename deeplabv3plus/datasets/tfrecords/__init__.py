fb4c90bff997c06d01f819165670796646ec01f1
Arindam Das <dasarindam.mails@gmail.com>
2021-03-19 21:31:50 +0530

    Removed incorrect import.

--- deeplabv3plus/datasets/tfrecords/__init__.py
+++ deeplabv3plus/datasets/tfrecords/__init__.py
@@ -1,4 +1,4 @@
 from .tfrecord_creator import TFRecordCreator
 from .augmentations import AugmentationFactory
-from .tfrecord_loader import TFRecordLoader, configure_dataset
+from .tfrecord_loader import TFRecordLoader
 from .tfrecord_dataset import TFRecordDataset
